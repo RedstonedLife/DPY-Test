4cf1a4ec46a32ed40f5e730d30d8ab56d886b41a
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2018-12-25 07:41:10 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
 
+bot = commands.Bot(command_prefix=os.environ['PREFIX'])
+# Getting Environ Vars os.environ['VAR_NAME']
+
+
+
+@bot.event
+async def on_ready():
+  print(bot.user.name)
+
+  
+#A SIMPLE TEST COMMAND
+@bot.command(pass_context=True)
+async def hi(ctx):
+  await bot.say("Hello there"+" "+ctx.message.author.name)
+  
+  
+ 
+bot.run(os.environ['TOKEN'])
